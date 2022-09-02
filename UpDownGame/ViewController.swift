f69a8a87c6355482668b67dc8b7d69c1dfc93670
yuvinrho <yvrho@naver.com>
2022-09-03 00:21:17 +0900

    4. 코딩 입문

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         slider.setThumbImage(UIImage(named: "slider_thumb"), for: .normal)
-        
+        reset()
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
@@ -28,6 +34,21 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 
 }
