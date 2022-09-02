e10dc8ebaf9e7eb239fa68a3983b36d0639b4d36
yuvinrho <yvrho@naver.com>
2022-09-02 12:03:23 +0900

    2. UI와 이벤트

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
 
 }
 
