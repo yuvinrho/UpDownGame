dbaea8892a58ca0d75b36c008e537fcbc4a61454
yuvinrho <yvrho@naver.com>
2022-09-03 01:33:42 +0900

    6. 화면전환

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 노유빈 on 2022/09/03.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
