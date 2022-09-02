4236221e1a3fdc47a6ea1f22f929c3dc4e417602
yuvinrho <yvrho@naver.com>
2022-09-03 01:01:55 +0900

    5. 코드로 기능 구현하기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var randomValue: Int = 0
     var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
@@ -26,10 +25,45 @@ class ViewController: UIViewController {
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default) { (action) in
+            self.reset()
+        }
+        
+        alert.addAction(okAction)
+        present(alert,
+                animated: true,
+                completion: nil)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            showAlert(message: "YOU HIT!!")
+            reset()
+        } else if tryCount >= 5 {
+            showAlert(message: "YOU LOSE...")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
